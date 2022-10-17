185af24c6e30c218ccfd2e4ab39ca01397a9ad95
jedwed <83894732+jedwed@users.noreply.github.com>
2022-10-17 23:28:20 +1100

    Added a form to add sample identiFunners

--- client/src/pages/Demo.js
+++ client/src/pages/Demo.js
@@ -1,24 +1,38 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+/**
+ * A demo component to showcase connecting with a backend server
+ */
 function Demo() {
   const [identiFunners, setIdentiFunners] = useState([]);
-  // const [text, setText] = useState('');
+  const [category, setCategory] = useState('');
+  const [categoryItem, setCategoryItem] = useState('');
+  const [rating, setRating] = useState(0);
+  // This success state is passed as a dependency for the useEffect
+  // Such that whenever a successful POST request to the server is made,
+  // useEffect fetches the data from the server so all is updated
+  const [success, setSuccess] = useState(false);
 
+  // Makes a request to the server when component mounts + when success state is set
   useEffect(() => {
     axios
       .get('http://localhost:8000/demo')
-      .then((response) => setIdentiFunners(response.data));
-    console.log('hello world');
-  }, []);
+      .then((response) => setIdentiFunners(response.data))
+      .then(setSuccess(false));
+  }, [success]);
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   axios.post('http//localhost:8000/demo', {
-  //     category: spgjerjg,
-  //     categoryItem,
-  //   });
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:8000/demo', {
+        category,
+        categoryItem,
+        rating,
+      })
+      // Set success state to re-fetch updated data
+      .then(setSuccess(true));
+  };
 
   return (
     <>
@@ -29,14 +43,28 @@ function Demo() {
           </li>
         ))}
       </ul>
-      {/* <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
+        <label>Category</label>
         <input
           type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        <label>Category Item</label>
+        <input
+          type="text"
+          value={categoryItem}
+          onChange={(e) => setCategoryItem(e.target.value)}
+        />
+        <label>Rating</label>
+        <input
+          type="number"
+          min={0}
+          label="Rating"
+          onChange={(e) => setRating(e.target.value)}
         />
         <button type="submit">Submit</button>
-      </form> */}
+      </form>
     </>
   );
 }
--- server/server.js
+++ server/server.js
@@ -4,7 +4,10 @@ import cors from 'cors';
 const app = express();
 const port = 8000;
 
-const dataStore = {
+// NOTE: This datastore is not complete
+// YOu might have a list of categories, and within each, nest a list of category items
+// Also, maybe come up with a name other than identiFunners (yea ik it's quite impossible but try your best)
+const sampleDataStore = {
   users: [],
   identiFunners: [
     {
@@ -25,29 +28,41 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  console.log(req.query.message);
   res.send({ message: 'Hello' });
 });
 
 app.post('/user/register', (req, res) => {
   const { email, username, password } = req.body;
-  console.log(email, username, password);
 });
 
+/**
+ * GET /demo
+ * Gets a list of dummy identiFunners from a sample data store
+ * @returns An array of identiFunners bruh idk how to do documentation :/
+ * DO NOT UNDER ANY CIRCUMSTANCES MODEL THIS FOR YOUR 1531 YOUR MARK WILL SINK
+ */
 app.get('/demo', (req, res) => {
-  console.log(dataStore.identiFunners);
-  res.json(dataStore.identiFunners);
+  res.json(sampleDataStore.identiFunners);
 });
 
+/**
+ * POST /demo
+ * Creates a new sample identiFunner
+ * @param The category name
+ * @param The category item
+ * @param The rating of the item
+ * @returns The userID
+ */
 app.post('/demo', (req, res) => {
+  // Get data from JSON body
   const { category, categoryItem, rating } = req.body;
   if (!category || !categoryItem || !rating) {
     res.status(500).json({ error: 'Please fill in all fields' });
   }
 
-  const newId = dataStore.totalIdentiFunners;
-  dataStore.totalIdentiFunners += 1;
-  dataStore.identiFunners.push({
+  const newId = sampleDataStore.totalIdentiFunners;
+  sampleDataStore.totalIdentiFunners += 1;
+  sampleDataStore.identiFunners.push({
     id: newId,
     category,
     categoryItem,
