af8f9ac9c5eee85333d4a84f334d1f8a103b9fb1
jedwed <83894732+jedwed@users.noreply.github.com>
2022-10-18 11:40:24 +1100

    Added tailwind css

--- client/src/pages/Demo.js
+++ client/src/pages/Demo.js
@@ -35,7 +35,8 @@ function Demo() {
   };
 
   return (
-    <>
+    <div>
+      <h1 className="text-blue-700 text-2xl font-bold">IdentiFunners</h1>;
       <ul>
         {identiFunners.map((item) => (
           <li>
@@ -44,28 +45,44 @@ function Demo() {
         ))}
       </ul>
       <form onSubmit={handleSubmit}>
-        <label>Category</label>
-        <input
-          type="text"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-        <label>Category Item</label>
-        <input
-          type="text"
-          value={categoryItem}
-          onChange={(e) => setCategoryItem(e.target.value)}
-        />
-        <label>Rating</label>
-        <input
-          type="number"
-          min={0}
-          label="Rating"
-          onChange={(e) => setRating(e.target.value)}
-        />
-        <button type="submit">Submit</button>
+        <div className="mb-4">
+          <label className="block">Category</label>
+          <input
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="shadow border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block">Category Item</label>
+          <input
+            type="text"
+            value={categoryItem}
+            onChange={(e) => setCategoryItem(e.target.value)}
+            className="shadow border rounded"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block">Rating</label>
+          <input
+            type="number"
+            min={0}
+            label="Rating"
+            onChange={(e) => setRating(e.target.value)}
+            className="shadow border rounded"
+          />
+        </div>
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 py-2 rounded"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
       </form>
-    </>
+    </div>
   );
 }
 
