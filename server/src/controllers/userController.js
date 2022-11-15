95c8a781289c5a42e7c7af876c08b8e8b631ca19
jedwed <83894732+jedwed@users.noreply.github.com>
2022-11-16 00:35:16 +1100

    Completed basic login and register

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,16 +2,40 @@ import User from '../models/userModel.js';
 
 const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
+  // TODO: Hash password (maybe using bcrypt)
   const user = await User.create({
     username,
     email,
     password,
   });
-  uId = user._id;
-  res.json({ uId });
+  // TODO: Create a token (perhaps use JWT tokens)
+  const token = 'epic token';
+
+  res.json({
+    uId: user._id,
+    username: user.username,
+    email: user.email,
+    password: user.password,
+    token: token,
+  });
 };
-const loginUser = (req, res) => {
-  const { username, email, password } = req.body;
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && password === user.password) {
+    const token = 'epic';
+    res.json({
+      uId: user._id,
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      token: token,
+    });
+  } else {
+    res.status(400).json({ error: 'bruh' });
+  }
 };
 
 export { registerUser, loginUser };
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-import express, { response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import itemRoutes from './routes/itemRoutes.js';
