0e5fe653aa4cb860065b6bc0ead7db35134486d1
istan18 <chriscla2003@gmail.com>
2022-12-04 00:15:19 +1100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import Demo from './pages/Demo';
-
+import ProfilePage from './pages/ProfilePage';
 function App() {
+  const [user, setUser] = useState();
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage setUser={setUser} />} />
+        <Route path="/register" element={<RegisterPage setUser={setUser} />} />
         <Route path="/demo" element={<Demo />} />
+        <Route path="/profile" element={<ProfilePage user={user} />} />
       </Routes>
     </BrowserRouter>
   );
