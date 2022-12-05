0248838b1d2ab1ac3830db62167f2d447882594b
Vincent <vincentle0402@hotmail.com>
2022-12-05 17:54:12 +1100

    changed footer navigation

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 function Footer() {
+  const navigate = useNavigate();
+
   return (
     <footer class="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-900">
       <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{' '}
-        <a href="https://flowbite.com/" class="hover:underline">
+        <span class="hover:underline" onClick={() => { navigate('/') }}>
           identiFun™
-        </a>
+        </span>
         . All Rights Reserved.
       </span>
     </footer>
