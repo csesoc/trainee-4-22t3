1e33d58aa526c3cdf2c64a17aad65370d1337f89
Vincent <vincentle0402@hotmail.com>
2022-12-06 15:32:37 +1100

    removed unnecessary imports

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import NavbarSearch from './NavbarSearch';
 import NavbarLogo from './NavbarLogo';
 
