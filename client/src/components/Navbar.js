5d0829865f358052d83da54df4664b235a968ef7
Vincent <vincentle0402@hotmail.com>
2022-12-06 14:58:49 +1100

    hide search results when search box is out of focus

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,6 +14,10 @@ function Navbar() {
     setInput(ev.target.value);
   }
 
+  const [focused, setFocused] = useState(false)
+  const onFocus = () => setFocused(true)
+  const onBlur = () => setFocused(false)
+
   useEffect(() => {
     axios
       .get('http://localhost:5000/users/search?searchStr=' + input)
@@ -26,10 +30,15 @@ function Navbar() {
       .catch((err) => {
         console.log(err);
       });
-    if (input === '') {
-      setDisplaySearches(false);
-    }
   }, [input]);
+
+  useEffect(() => {
+    if (input === '' || focused === false) {
+      setDisplaySearches(false);
+    } else {
+      setDisplaySearches(true);
+    }
+  }, [input, focused]);
 
   return (
     <section>
@@ -53,7 +62,7 @@ function Navbar() {
                   <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                   </div>
-                  <input type="search" id="default-search" onChange={handleOnChange} class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search users" required />
+                  <input type="search" id="default-search" onChange={handleOnChange} onFocus={onFocus} onBlur={onBlur} class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search users" required />
               </div>
             </form>
             <div id="dropdown" class="absolute my-1 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
