76f8255b652db535d384b19370331cd413780482
Vincent <vincentle0402@hotmail.com>
2022-12-06 14:35:36 +1100

    removed scrollbar in categories

--- client/src/components/HomePageCategory.js
+++ client/src/components/HomePageCategory.js
@@ -4,7 +4,7 @@ function HomePageCategory({ userData, category }) {
   return (
     <div class="flex flex-col bg-white m-auto p-auto my-4 rounded-md">
       <h2 class="flex py-4 px-2 mx-5 font-semibold text-2xl">{category}</h2>
-      <div class="flex overflow-x-scroll pb-2 hide-scroll-bar">
+      <div class="flex overflow-x-scroll pb-2 scrollbar-hide">
         <div class="flex flex-nowrap ml-10 justify-start">
           {userData.items[category].map((item) => (
             <HomePageItem item={item} />
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.scrollbar-hide': {
+          /* IE and Edge */
+          '-ms-overflow-style': 'none',
+
+          /* Firefox */
+          'scrollbar-width': 'none',
+
+          /* Safari and Chrome */
+          '&::-webkit-scrollbar': {
+            display: 'none'
+          }
+        }
+      }
+      )
+    })
+  ]
 };
