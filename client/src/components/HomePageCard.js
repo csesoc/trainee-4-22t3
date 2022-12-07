652d9ab3f8fbffe14ae96351f565823bf4e56158
Vincent <vincentle0402@hotmail.com>
2022-12-07 21:20:42 +1100

    changed colouring of homepage

--- client/src/components/HomePageCard.js
+++ client/src/components/HomePageCard.js
@@ -5,7 +5,7 @@ function HomePageCard({ userData }) {
   const navigate = useNavigate();
   // w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700
   return (
-    <div className="max-w-md w-full mx-4 mb-2 rounded-lg shadow-lg bg-gray-800 border border-gray-700 ">
+    <div className="max-w-md w-full mx-4 mb-2 rounded-lg shadow-lg bg-blue-500/10 border border-white ">
       <div className="px-6 py-4">
         <button
           onClick={() => navigate('/profile/' + userData.user.username)}
--- client/src/components/HomePageItem.js
+++ client/src/components/HomePageItem.js
@@ -3,7 +3,7 @@ import defaultImg from '../images/default.jpg';
 function HomePageItem({ item }) {
   return (
     <div class="inline-flex">
-      <div className="w-64 h-100 max-w-xs overflow-hidden rounded-lg bg-gray-700">
+      <div className="w-64 h-100 max-w-xs overflow-hidden rounded-lg bg-gray-900">
         <img
           src={item.imageUrl}
           alt="item"
@@ -12,8 +12,8 @@ function HomePageItem({ item }) {
           }}
           class="flex-none md:w-2/3 rounded-lg h-64 object-cover transition ease-in-out hover:shadow-xl duration-200"
         />
-        <p class="text-white font-semibold">{item.name}</p>
-        <p class="text-white font-semibold">{item.rating}/10</p>
+        <p class="text-white font-normal">{item.name}</p>
+        <p class="text-white font-normal">{item.rating}/10</p>
       </div>
     </div>
   );
