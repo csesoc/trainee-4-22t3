7c9a361dfd7f83be8c2812b17f7a5018ccbae6a2
Vincent <vincentle0402@hotmail.com>
2022-12-07 21:03:05 +1100

    navbar colour and button changes to give link rather than navigate

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,15 +12,11 @@ function Navbar({ user, setUser }) {
           <NavbarSearch />
           <div>
             {!user ? (
-              <button
-                onClick={() => {
-                  navigate('/login');
-                }}
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                title="Login"
-              >
-                Login
-              </button>
+                <a 
+                  href="http://localhost:3000/login"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  title="Login"
+                >Login</a>
             ) : (
               <button
                 onClick={() => {
--- client/src/components/NavbarLogo.js
+++ client/src/components/NavbarLogo.js
@@ -4,21 +4,17 @@ function NavbarLogo() {
   const navigate = useNavigate();
 
   return (
-    <div
-      onClick={() => {
-        navigate('/');
-      }}
-      class="flex items-center cursor-pointer"
-      title="identiFun"
-    >
-      <img
-        src="https://i.imgur.com/ylqbx7Y.png"
-        class="w-14 h-14"
-        alt="identiFun logo"
-      />
-      <span class="text-xl font-semibold dark:text-white">
-        identiFun
-      </span>
+    <div title="identiFun">
+      <a href="http://localhost:3000/" class="flex items-center">
+        <img
+          src="https://i.imgur.com/ylqbx7Y.png"
+          class="w-14 h-14"
+          alt="identiFun logo"
+        />
+        <span class="text-xl font-semibold dark:text-white">
+          identiFun
+        </span>
+      </a>
     </div>
   );
 }
