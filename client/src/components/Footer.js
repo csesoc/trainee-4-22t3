20f9d31057248bab8406973c5022458677a5fffa
Vincent <vincentle0402@hotmail.com>
2022-12-07 20:03:59 +1100

    made footer smaller

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="p-4 shadow md:px-6 md:py-8 bg-[#252526]">
+    <footer className="p-4 shadow md:px-6 md:py-4 bg-[#252526]">
       <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022 <span className="hover:underline">identiFun™</span>. All Rights
         Reserved.
